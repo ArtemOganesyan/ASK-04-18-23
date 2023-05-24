11cb24d6661c64ccc5cb800fdaca3a072367739c
KlumsyK <konstantin.pogorelov@gmail.com>
2023-05-23 18:29:28 -0700

    adding changes: ASK_Components_KP.java ASK_Defs_KP.java ComponentTests.feature Quizzes_Sliders.feature ASK041823-210.feature
    pom.xml changes (definitions for mysql and JSONObject)

---
+++ src/test/java/support/ASK_Components_KP.java
@@ -0,0 +1,71 @@
+package support;
+
+public class ASK_Components_KP {
+
+//    Artem's component (modded)
+    public static String componentFor(String name) {
+        switch (name) {
+
+//      Login Page Elements
+
+            case "LoginEmailField":
+                return "//input[@formcontrolname='email']";
+
+            case "LoginPasswdField":
+                return "//input[@formcontrolname='password']";
+
+            case "SubmitButtn":
+                return "//button[@type='submit']";
+
+            case "ForgotPass":
+                return "//a[@href='/#/forgot-password']";
+
+            case "RegisterNow":
+                return "//button/span[contains(text(),'Register Now')]";
+
+
+//      Home Page Elements
+
+//            Navigation Panel
+
+            case "LogOut":
+                return "//h5[contains(text(),'Log Out')]";
+
+            case "Home":
+                return "//a[@href='/#/home']";
+
+            case "Submissions":
+                return "//h5[text(),'Submissions']";
+
+            case "Assignments":
+                return "//h5[text()='Assignments']";
+
+            case "Quizzes":
+                return "//h5[text()='Quizzes']";
+
+            case "UsersMgmt":
+                return "//h5[text()=\"User's Management\"]";
+
+            case "Settings":
+                return "//h5[contains(text(),'Settings')]";
+
+//                Notification Panels Links
+
+            case "GoToSubmissions":
+                return "//h5[contains(text(),'Go To Submissions')]";
+
+            case "GoToAssignments":
+                return "//h5[contains(text(),'Go To Assignments')]";
+
+            case "GoToUsersMgmt":
+                return "//h5[contains(text(),'Go To Users Management')]";
+
+            case "QuizzesList":
+                return "//div[@class='quizzes']";
+
+        }
+
+        return "";
+    }
+
+}
