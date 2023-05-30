86f2122c7932c69c0bc94a909eef1385d57997ef
Artem Oganesyan <artem.oganesyan@gmail.com>
2023-05-30 16:39:14 -0700

    upd

--- src/test/java/support/TestContext.java
+++ src/test/java/support/TestContext.java
@@ -32,7 +32,7 @@ public class TestContext {
     }
 
     public static void initialize() {
-        initialize("chrome", "local", true);
+        initialize("chrome", "local", false);
     }
 
     public static void teardown() {
@@ -54,11 +54,6 @@ public class TestContext {
                     chromePreferences.put("password_manager_enabled", false);
                     ChromeOptions chromeOptions = new ChromeOptions();
                     chromeOptions.addArguments("--start-maximized");
-//                    chromeOptions.addArguments("--headless");
-//                    chromeOptions.addArguments("--no-sandbox");
-//                    chromeOptions.addArguments("--disable-dev-shm-usage");
-//                    chromeOptions.addArguments("--disable-gpu");
-//                    chromeOptions.addArguments("--disable-software-rasterizer");
                     chromeOptions.setExperimentalOption("prefs", chromePreferences);
                     System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
                     if (isHeadless) {
